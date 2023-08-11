3d125178b6771b437446f96e6e1b1b30e4c2c7c6
sehr <sehr.sehr1112@gmail.com>
2023-08-12 00:06:48 +0500

    Create room.js

---
+++ models/room.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const roomSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    maxcount:{
+        type:Number,
+        required:true
+    },
+    phonenumber:{
+        type:Number,
+        required:true
+    },
+    rentperday:{
+        type:Number,
+        required:true
+    },
+    imageUrl:[],
+    currentbookings:[],
+    type:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    timestamps : true,
+})
+
+const roomModel = mongoose.model('room',roomSchema);
+module.exports = roomModel;
