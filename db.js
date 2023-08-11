3b92bac0ff0f0265adca72781c77b4091974d795
sehr <sehr.sehr1112@gmail.com>
2023-08-11 23:25:09 +0500

    Upfate all files

---
+++ db.js
@@ -0,0 +1,10 @@
+ const mongoose = require('mongoose');
+require('dotenv').config();
+var MONGODB_URI = process.env.MONGODB_URI;
+console.log("HEllo Wondera 😷 "+MONGODB_URI);
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+var connection = mongoose.connection;
+connection.on('error',()=>{console.log('😔 error connecting to db')});
+connection.on('connected',()=>{console.log('❤️  ❤️  ❤️    Connected to db')});
+
+module.exports = mongoose;
