987feb46e0f8366e05e2899615a646fc33982033
sehr <sehr.sehr1112@gmail.com>
2023-08-14 10:36:31 +0500

    config vercel

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
  const mongoose = require('mongoose');
 require('dotenv').config();
 var MONGODB_URI = process.env.MONGODB_URI;
-console.log("HEllo Wondera 🕊️  🕊️  🕊️  "+MONGODB_URI);
-mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+ mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 var connection = mongoose.connection;
 connection.on('error',()=>{console.log('😔 error connecting to db')});
 connection.on('connected',()=>{console.log('❤️  ❤️  ❤️    Connected to db')});
